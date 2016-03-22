bb1e33a614958b3ba0eb9a72a28560c760f6eaff
Blaž Dvorjak <blaz.dvo@gmail.com>
2016-03-22 10:43:31 +0100

    popravek zaporedja

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,14 @@
 i = int(raw_input("Vnesi stevilo med 1 in 100: "))
 if i > 100:
-    print("Vnesli ste preveliko stevilko!")
+    print("Vnesli ste preveliko stevilo!")
 else:
-    for x in range(1,i + 1):
-        if (x % 3 != 0) and (x % 5 != 0) :
+    for x in range(1,i+1):
+        if (x % 3 != 0) and (x % 5 != 0):
             print x
+        if (x % 3 == 0) and (x % 5 == 0):
+            print "fizzbuzz"
         elif (x % 3 == 0):
             print "fizz"
         elif (x % 5 == 0):
             print "buzz"
-        if (x % 3 == 0) and (x % 5 == 0):
-            print "fizzbuzz"
+
